0c4e4f2c419030741c7b6d9293fdb44588722b05
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-05-07 20:06:59 +0900

    feat: integrate json-server for quiz data and enhance app structure
    - Added json-server as a dependency and configured a script to run it.
    - Created a new MainContent component to encapsulate main content structure.
    - Updated App component to fetch questions from the json-server and manage state with useReducer.
    - Refactored Header component for improved readability.
    - Added a logo image to the public directory.
    - Cleaned up CSS by removing duplicate font import.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import React from "react";
-import DateCounter from "./DateCounter.jsx";
+import React, { useEffect, useReducer } from "react";
+import Header from "./Header.jsx";
+import MainContent from "./MainContent.jsx";
+
+const initialState = {
+  question: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    default:
+      throw new Error("挙動がわかりません");
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "ready":
+      return { ...state, status: "ready" };
+    case "active":
+      return { ...state, status: "active" };
+    case "finished":
+      return { ...state, status: "finished" };
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <MainContent>
+        <p>1/15</p>
+        <p>質問は？</p>
+      </MainContent>
     </div>
   );
 }
